fc9d122dd954e2b66a1a81c808b79f39362eb655
shine <null>
2019-09-05 00:37:05 +0800

    使用队列实现栈

---
+++ stack/src/main/java/com/shine/leetcode/Test225.java
@@ -0,0 +1,66 @@
+package com.shine.leetcode;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * description: 使用队列实现一个栈
+ * @author shine
+ * @date 2019-09-04 23:58
+ * @version 1.0
+ */
+public class Test225 {
+	Queue<Integer> tempQueue = new LinkedList<>();
+	Queue<Integer> dataQueue = new LinkedList<>();
+	public Test225() {
+
+	}
+
+	/** Push element x onto stack. */
+	public void push(int x) {
+		tempQueue.add(x);
+		while(!dataQueue.isEmpty()){
+			tempQueue.add(dataQueue.remove());
+		}
+		dataQueue = tempQueue;
+		tempQueue = new LinkedList<>();
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	public int pop() {
+		Integer peek=-1;
+		if(!dataQueue.isEmpty()){
+			 peek = dataQueue.peek();
+			dataQueue.remove();
+		}
+		return peek;
+	}
+
+	/** Get the top element. */
+	public int top() {
+		if(!dataQueue.isEmpty()){
+			return dataQueue.peek();
+		}
+		return -1;
+	}
+
+	/** Returns whether the stack is empty. */
+	public boolean empty() {
+		return dataQueue.isEmpty();
+	}
+
+
+	@Test
+	public void test01(){
+		Test225 stack = new Test225();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		int pop1 = stack.pop();
+		int pop2 = stack.pop();
+		int pop3 = stack.pop();
+		System.out.println(pop3);
+	}
+}
