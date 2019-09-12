c52b76ff1c99577777bb9b1a43da27ec73d2ac60
shine <null>
2019-09-12 16:38:36 +0800

    有重复元素求子集

---
+++ other-algo/src/main/java/com/shine/array/leetcode/Test90.java
@@ -0,0 +1,63 @@
+package com.shine.array.leetcode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * DESCRIPTION: 有重复元素的集合求不重复的子集个数
+ * 首先对原数组进行排序，然后求子集，对最后的结果用set去重
+ *
+ * @author Shine
+ * @create 9/12/2019 3:58 PM
+ */
+public class Test90 {
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+		Arrays.sort(nums); // 默认从小到大排序
+		List<List<Integer>> result = new ArrayList<>();
+		List<Integer> item = new ArrayList<>();
+		result.add(item);
+		generate(0, nums, item, result);
+
+		Set<List<Integer>> set = new HashSet<>(result); // 使用set去重
+		return new ArrayList<>(set); // 将结果转换成List返回
+	}
+
+	public void generate(int i, int[] nums, List<Integer> item, List<List<Integer>> result) {
+		if (i >= nums.length) return;
+		item.add(nums[i]);
+		result.add(new ArrayList<>(item));
+		generate(i + 1, nums, item, result);
+		item.remove(item.size() - 1);
+		generate(i + 1, nums, item, result);
+	}
+
+
+	public List<List<Integer>> subsetsWithDup2(int[] nums) {
+		Arrays.sort(nums); // 默认从小到大排序
+		List<List<Integer>> result = new ArrayList<>();
+		int size = 1 << nums.length;
+		for (int i = 0; i < size; i++) {
+			List<Integer> item = new ArrayList<>();
+			for (int j = 0; j < nums.length; j++) {
+				if ((i & (1 << j)) != 0) {
+					item.add(nums[j]);
+				}
+			}
+			result.add(item);
+		}
+		Set<List<Integer>> set = new HashSet<>(result); // 使用set去重
+		return new ArrayList<>(set); // 将结果转换成List返回
+	}
+
+	@Test
+	public void test01() {
+		int[] nums = {3, 1, 2, 2};
+		List<List<Integer>> lists = subsetsWithDup2(nums);
+		System.out.println(lists.toString());
+	}
+}
