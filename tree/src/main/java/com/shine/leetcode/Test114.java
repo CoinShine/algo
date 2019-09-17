987f875d5c11ac41305b1cb3fc0a9a8c0447539a
shine <null>
2019-09-17 18:36:03 +0800

    二叉树宽搜

---
+++ tree/src/main/java/com/shine/leetcode/Test114.java
@@ -0,0 +1,65 @@
+package com.shine.leetcode;
+
+import org.junit.Test;
+
+/**
+ * DESCRIPTION:二叉树原地转链表
+ * 单链表仍然使用树的结构，left=null right=next
+ * @author Shine
+ * @create 2019/9/17 15:48
+ */
+public class Test114 {
+	public void flatten(TreeNode root) {
+
+		while (root != null) {
+			//左子树为 null，直接考虑下一个节点
+			if (root.left == null) {
+				root = root.right;
+			} else {
+				// 找左子树最右边的节点
+				TreeNode pre = root.left;
+				while (pre.right != null) {
+					pre = pre.right;
+				}
+				//将原来的右子树接到左子树的最右边节点
+				pre.right = root.right;
+				// 将左子树插入到右子树的地方
+				root.right = root.left;
+				root.left = null;
+				// 考虑下一个节点
+				root = root.right;
+			}
+		}
+	}
+
+
+	@Test
+	public void test01(){
+		TreeNode a = new TreeNode(1);
+		TreeNode b = new TreeNode(2);
+		TreeNode c = new TreeNode(5);
+		TreeNode d = new TreeNode(3);
+		TreeNode e = new TreeNode(4);
+		TreeNode f = new TreeNode(6);
+		a.left=b;
+		a.right=c;
+		//b.left=d;
+		//b.right=e;
+		//c.right=f;
+		flatten(a);
+	}
+}
+
+/**
+ * Definition for a binary tree node.
+ */
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
+
---
+++ tree/src/main/java/com/shine/leetcode/Test199.java
@@ -0,0 +1,73 @@
+package com.shine.leetcode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * DESCRIPTION:二叉树右视图
+ * 借助队列使用广搜，输入每层最后一个节点
+ *
+ * @author Shine
+ * @create 2019/9/17 16:59
+ */
+public class Test199 {
+	private Queue<TreeNode> nodeQueue = new LinkedList<>(); // 定义队列，存储广搜结果
+	private Queue<Integer> depthQueue = new LinkedList<>(); // 定义广搜每个节点对应的层数
+
+	public List<Integer> rightSideView(TreeNode root) {
+		List<Integer> result = new ArrayList<>();
+		if (root != null) {
+			nodeQueue.add(root);
+			depthQueue.add(0);
+		}
+		while (!nodeQueue.isEmpty()) {
+			TreeNode node = nodeQueue.poll(); // nodeQueue和depthQueue是一一对应的
+			int depth = depthQueue.poll();
+
+			if (result.size() == depth) { // 遍历每层第一节点时放入
+				result.add(node.val);
+			} else {
+				result.set(depth, node.val); // 将每层右边的节点更新到结果中
+			}
+			if (node.left != null) {
+				nodeQueue.add(node.left);
+				depthQueue.add(depth + 1);
+			}
+			if (node.right != null) {
+				nodeQueue.add(node.right);
+				depthQueue.add(depth + 1);
+			}
+		}
+		return result;
+	}
+
+	@Test
+	public void test01() {
+		TreeNode a = new TreeNode(1);
+		TreeNode b = new TreeNode(2);
+		TreeNode c = new TreeNode(3);
+		TreeNode d = new TreeNode(5);
+		TreeNode e = new TreeNode(4);
+		a.left = b;
+		a.right = c;
+		b.right = d;
+		c.right = e;
+		List<Integer> integers = rightSideView(a);
+		System.out.println(integers.toString());
+	}
+
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
