ed111208a701a3328a6a2bbe8fde35e2716546f1
shine <null>
2019-09-18 18:48:35 +0800

    快排

--- sort/src/main/java/com/shine/geektime/QuickSort.java
+++ sort/src/main/java/com/shine/geektime/QuickSort.java
@@ -1,58 +1,54 @@
 package com.shine.geektime;
 
+import java.util.Arrays;
+
 /**
  * description: 快排
+ *
  * @author shine
- * @date 2019/08/29 15:27
  * @version 1.0
+ * @date 2019/08/29 15:27
  */
 public class QuickSort {
 
-	private static void sort(int[] a,int lo,int  hi){
-		if(lo>=hi){
-			return;
-		}
-		int q = quickSort(a,lo,hi);
-		sort(a, lo, q-1);
-		sort(a,q+1, hi);
+	// 快速排序，a是数组，n表示数组的大小
+	public static void quickSort(int[] a, int n) {
+		quickSortInternally(a, 0, n - 1);
 	}
 
-	private static int quickSort(int[] a,int lo,int  hi){
-
-		//三数取中,防止超过int最大值
-		int mid=lo+((hi-lo)>>1);
-		if(a[mid]>a[hi]){
-			swap(a[mid],a[hi]);
+	// 快速排序递归函数，p,r为下标
+	private static void quickSortInternally(int[] a, int l, int r) {
+		if (l >= r) return;
+		int q = partition(a, l, r); // 获取分区点 一次分区后 左侧小于基准数 右侧大于基准数
+		quickSortInternally(a, l, q - 1); // 分治
+		quickSortInternally(a, q + 1, r);
+	}
+	/**
+	 * 挖坑填坑
+	 *
+	 * @param a
+	 * @param l
+	 * @param r
+	 * @return
+	 */
+	private static int partition(int[] a, int l, int r) {
+		int i = l, j = r;
+		int x = a[i]; // 基准数，可以选择头尾中三数取中
+		while (i < j) {
+			while (i < j && a[j] >= x) j--; // 从右侧遍历填坑
+			if (i < j) a[i++] = a[j];
+			while (i < j && a[i] < x) i++; //从左侧遍历填坑
+			if (i < j) a[j--] = a[i];
 		}
-		if(a[lo]>a[hi]){
-			swap(a[lo],a[hi]);
-		}
-		if(a[mid]>a[lo]){
-			swap(a[mid],a[lo]);
-		}
-
-		int key = a[lo];
-		while(lo<hi){
-			while(a[hi]>key&&lo<hi){
-				hi--;
-			}
-			a[lo] = a[hi];
-			while(a[lo]<key&& lo<hi){
-				lo++;
-			}
-			a[hi]=a[lo];
-		}
-		a[hi] = key;
-		return hi;
+		// 退出时i==j
+		a[i] = x;
+		return i;
 	}
 
-	public static void swap(int a,int b){
-		int temp=a;
-		a=b;
-		b=temp;
-	}
-	public static void main(String[] args){
-	  int[] a = {23,46,0,8,11,18};
-	  sort(a, 0, 5);
+	public static void main(String[] args) {
+		int[] a = {23, 46, 0, 8, 11, 18};
+		//int[] b = {6, 6, 6, 6, 6, 6};
+		quickSort(a, 6);
+		System.out.println(Arrays.toString(a));
 	}
 }
