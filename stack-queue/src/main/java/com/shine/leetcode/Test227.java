6e57cafbd73a8fc35a8b3c9f36eddcbcfe8bc04f
shine <null>
2019-11-07 17:23:32 +0800

    求二叉树的最大和最小深度

--- stack-queue/src/main/java/com/shine/leetcode/Test227.java
+++ stack-queue/src/main/java/com/shine/leetcode/Test227.java
@@ -29,8 +29,8 @@ public class Test227 {
 		char sign = '+';
 		Stack<Integer> nums = new Stack<>();
 		for (int i = 0; i < s.length(); ++i) {
-			if (s.charAt(i) >= '0') {
-				d = d * 10 - '0' + s.charAt(i);
+			if (s.charAt(i) >= '0') { //加减乘除和空格ASCII码都小于'0'
+				d = d * 10 - '0' + s.charAt(i);//进位(先减法)
 			}
 			if ((s.charAt(i) < '0' && s.charAt(i) != ' ') || i == s.length() - 1) {
 				if (sign == '+') {
