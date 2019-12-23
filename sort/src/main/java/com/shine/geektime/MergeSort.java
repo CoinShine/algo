ee887c6f7c14af744f60fe5b7d1ebeae3bc775f7
shine <null>
2019-12-24 00:40:10 +0800

    归并排序

--- sort/src/main/java/com/shine/geektime/MergeSort.java
+++ sort/src/main/java/com/shine/geektime/MergeSort.java
@@ -1,6 +1,10 @@
 package com.shine.geektime;
 
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 /**
  * description: 归并排序
  * @author shine
@@ -11,55 +15,52 @@ public class MergeSort {
 
 
 	// 归并排序算法, a是数组，n表示数组大小
-	public static void mergeSort(int[] a, int n) {
-		mergeSortInternally(a, 0, n - 1);
+	public  int[] mergeSort(int[] a) {
+		return mergeSortInternally(a, 0, a.length-1);
 	}
 
 	// 递归调用函数
-	private static void mergeSortInternally(int[] a, int p, int r) {
+	private  int[] mergeSortInternally(int[] a, int low, int high) {
 		// 递归终止条件
-		if (p >= r) return;
-
+		if (low == high) return new int[]{a[low]};
 		// 取p到r之间的中间位置q,防止（p+r）的和超过int类型最大值
-		int q = p + ((r - p) >> 1);
+		int mid = low + ((high - low) >> 1);
 		// 分治递归
-		mergeSortInternally(a, p, q);
-		mergeSortInternally(a, q + 1, r);
-
+		int[] left = mergeSortInternally(a, low, mid);
+		int[] right = mergeSortInternally(a, mid + 1, high);
 		// 将A[p...q]和A[q+1...r]合并为A[p...r]
-		merge(a, p, q, r);
+		return merge(left,right);
 	}
 
-	private static void merge(int[] a, int p, int q, int r) {
-		int i = p;
-		int j = q + 1;
+	private int[] merge(int[] left,int[] right) {
+		int i = 0,j=0;
+		int l1 = left.length;
+		int l2 = right.length;
 		int k = 0; // 初始化变量i, j, k
-		int[] tmp = new int[r - p + 1]; // 申请一个大小跟a[p...r]一样的临时数组
-		while (i <= q && j <= r) {
-			if (a[i] <= a[j]) {
-				tmp[k++] = a[i++]; // i++等于i:=i+1
+		int[] tmp = new int[l1+l2]; // 申请一个大小跟a[p...r]一样的临时数组
+		while (i < l1 && j < l2) {
+			if (left[i] <= right[j]) {
+				tmp[k++] = left[i++]; // i++等于i:=i+1
 			} else {
-				tmp[k++] = a[j++];
+				tmp[k++] = right[j++];
 			}
 		}
 
-		// 判断哪个子数组中有剩余的数据
-		int start = i;
-		int end = q;
-		if (j <= r) {
-			start = j;
-			end = r;
+		while (i<l1){
+			tmp[k++] = left[i++];
 		}
-
-		// 将剩余的数据拷贝到临时数组tmp
-		while (start <= end) {
-			tmp[k++] = a[start++];
+		while (j<l2){
+			tmp[k++] = right[j++];
 		}
-
-		// 将tmp中的数组拷贝回a[p...r]
-		for (i = 0; i <= r - p; ++i) {
-			a[p + i] = tmp[i];
-		}
+		return tmp;
 	}
 
+	@Test
+	public void test01(){
+		int[] nums = {8,9,1,10,5,3,2,4,7,6};
+		int[] a = {1,3,5,6};
+		int[] b = {2,4};
+		int[] ints = mergeSort(nums);
+		System.out.println(Arrays.toString(ints));
+	}
 }
