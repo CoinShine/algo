0bc1a83a28d9f4a63effeb832f43b080549913f7
shine <null>
2021-10-23 15:14:44 +0800

    格式化

--- dynamic-pro/src/main/java/com/shine/leetcode/Test53.java
+++ dynamic-pro/src/main/java/com/shine/leetcode/Test53.java
@@ -4,7 +4,7 @@ package com.shine.leetcode;
  * description:最大子序和
  * 给定一个整数数组 nums ，找到一个具有最大和的连续
  * 子数组（子数组最少包含一个元素），返回其最大和。
- *
+ * <p>
  * 示例:
  * 输入: [-2,1,-3,4,-1,2,1,-5,4],
  * 输出: 6
@@ -25,8 +25,8 @@ public class Test53 {
 		dp[0] = nums[0];
 		int result = dp[0];
 		for (int i = 1; i < nums.length; i++) {
-			dp[i] = Math.max(dp[i-1]+nums[i],nums[i]);
-			if(dp[i]>result) result=dp[i];
+			dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+			if (dp[i] > result) result = dp[i];
 		}
 		return result;
 	}
